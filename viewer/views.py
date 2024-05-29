0abbff03978aad9aff1dbf52f1b04a898a066df1
patrik <patrik2540@seznam.cz>
2024-05-29 14:07:02 +0200

    CBV - list of movies.

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, ListView
 
 from viewer.models import *
 
@@ -59,4 +61,93 @@ def genre(request, pk):
     genre = Genre.objects.get(id=pk)
     movies = Movie.objects.filter(genre__id=pk)
     context = {'genre': genre, 'movies': movies}
-    return render(request, "movies_by_genres.html", context)
+    return render(request, "movies_by_genre.html", context)
+
+
+# CBV: Class-Based Views
+## View class
+class MoviesView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        context = {'movies': movies}
+        return render(request, "movies.html", context)
+
+
+## TemplateView class
+class MoviesTemplateView(TemplateView):
+    template_name = "movies.html"
+    extra_context = {'movies': Movie.objects.all()}
+
+
+## ListView class
+class MoviesListView(ListView):
+    template_name = "movies2.html"
+    model = Movie
+    # pozor: do template se posílá jako object_list
+
+
+# přepsat všechna stávající funkční view (mimo hello...) na CBV:
+# movie
+class MovieView(View):
+    def get(self, request, pk):
+        if Movie.objects.filter(id=pk).exists():
+            movie = Movie.objects.get(id=pk)
+            context = {'movie': movie}
+            return render(request, "movie.html", context)
+        return movies(request)
+
+
+class MovieTemplateView(TemplateView):
+    template_name = "movie.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+        context["movie"] = Movie.objects.get(id=pk)
+        return context
+
+
+# genres
+class GenresView(View):
+    def get(self, request):
+        genres = Genre.objects.all()
+        context = {'genres': genres}
+        return render(request, "genres.html", context)
+
+
+class GenresTemplateView(TemplateView):
+    template_name = "genres.html"
+    extra_context = {'genres': Genre.objects.all()}
+
+
+class GenresListView(ListView):
+    template_name = "genres.html"
+    model = Genre
+
+    # 1. způsob, jak předefinovat 'object_list' v contextu:
+    """def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['genres'] = Genre.objects.all()
+        return context"""
+
+    # 2. způsob, jak předefinovat 'object_list' v contextu:
+    context_object_name = 'genres'
+
+
+# genre
+class GenreView(View):
+    def get(self, request, pk):
+        genre = Genre.objects.get(id=pk)
+        movies = Movie.objects.filter(genre__id=pk)
+        context = {'genre': genre, 'movies': movies}
+        return render(request, "movies_by_genre.html", context)
+
+
+class GenreTemplateView(TemplateView):
+    template_name = "movies_by_genre.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+        context["movies"] = Movie.objects.filter(genre__id=pk)
+        return context
